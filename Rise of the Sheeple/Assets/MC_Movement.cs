23a7d206b95278fbcf0bea2c07a18db448b9b253
benc33 <60461291+benc33@users.noreply.github.com>
2020-10-29 18:19:17 -0600

    Base code made.

---
+++ Rise of the Sheeple/Assets/MC_Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_Movement : MonoBehaviour
+{
+
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1300;
+    public float moveX;
+
+    // Start is called before the first frame update
+    /*
+    void Start()
+    {
+        
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+
+    }
+
+    void PlayerMove()
+    {
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        //Animations?
+
+        //PlayerDirection
+        if((moveX < 0.0f) && (facingRight == false))
+        {
+            FlipPlayer ();
+        }
+        else if ((moveX > 0.0f) && (facingRight == true))
+        {
+            FlipPlayer();
+        }
+        //Physics should already be taken care of
+        gameObject.GetComponent<RigidBody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<RigidBody2D>().velocity.y);
+
+    }
+
+    void FlipPlayer()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+}
