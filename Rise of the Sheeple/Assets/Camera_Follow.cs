cd40085b512436f50af798bcfaec16ea238cd5ae
Isaiah Adams-Hart <ada16011@byui.edu>
2020-11-11 11:58:40 -0700

    Added Camera Script

---
+++ Rise of the Sheeple/Assets/Camera_Follow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y,player.position.z);
+    }
+}
