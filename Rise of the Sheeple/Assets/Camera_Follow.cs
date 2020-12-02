4708f72aef5f3cd7a77b4857ef7e7ebabc2267d1
Isaiah Adams-Hart <ada16011@byui.edu>
2020-12-01 17:29:57 -0700

    Added an updated camera and respawning

--- Rise of the Sheeple/Assets/Camera_Follow.cs
+++ Rise of the Sheeple/Assets/Camera_Follow.cs
@@ -2,13 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_Follow : MonoBehaviour
-{
-    public Transform player;
+public class Camera_Follow : MonoBehaviour {
 
-    // Update is called once per frame
-    void FixedUpdate ()
-    {
-        transform.position = new Vector3(player.position.x, player.position.y,player.position.z);
+    public Transform target;
+    public float damping = 1;
+    public float lookAheadFactor = 3;
+    public float lookAheadReturnSpeed = 0.5f;
+    public float lookAheadMoveThreshold = 0.1f;
+    public float yRestrictionPos = -1;
+
+    float offsetZ;
+    Vector3 lastTargetPosition;
+    Vector3 currentVelocity;
+    Vector3 lookAheadPos;
+
+    float nextTimeToSearch = 0;
+
+    // INit
+    void Start () {
+        lastTargetPosition = target.position;
+        offsetZ = (transform.position - target.position).z;
+        transform.parent = null;
+    }
+
+    // update
+    void Update () {
+
+        if (target == null) {
+            FindPlayer ();
+            return;
+        }
+            
+
+        // lookahead updated only is accelerating or change diretion
+        float xMoveDelta = (target.position - lastTargetPosition).x;
+
+        bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+        if (updateLookAheadTarget) {
+            lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+        } else {
+            lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+        }
+
+        Vector3 aheadTargetpos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+        Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetpos, ref currentVelocity, damping);
+
+        newPos = new Vector3 (newPos.x, Mathf.Clamp (newPos.y, yRestrictionPos, Mathf.Infinity), newPos.z);
+
+        transform.position = newPos;
+        lastTargetPosition = target.position;
+    }
+
+    void FindPlayer () {
+        if (nextTimeToSearch <= Time.time) {
+            GameObject searchResult = GameObject.FindGameObjectWithTag ("Player");
+            if (searchResult != null)
+                target = searchResult.transform;
+            nextTimeToSearch = Time.time + 0.5f;
+        }
     }
 }
---
+++ Rise of the Sheeple/Assets/GameMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+
+    void Start () {
+        if (gm == null) {
+            gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer () {
+
+        yield return new WaitForSeconds (spawnDelay);
+
+        Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+    
+    public static void  KillPlayer (Player player) {
+        Destroy (player.gameObject);
+        gm.StartCoroutine (gm.RespawnPlayer());
+    }
+
+}
